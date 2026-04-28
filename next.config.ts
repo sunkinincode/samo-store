602c2eaf242f5f81bee9b6a248d35495f31e0d04
Koravit Kessaro <koravit.kessaro.pgm@gmail.com>
2026-04-29 01:24:06 +0700

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* กำหนดให้ใช้ Edge Runtime ในจุดที่จำเป็น และลดข้อผิดพลาดในการ Build ลง Cloudflare Pages */
   images: {
-    unoptimized: true, // Cloudflare Pages มักจะมีปัญหากับ Image Optimization ของ Next.js
+    unoptimized: true,
   },
   experimental: {
     // ฟีเจอร์ลดขนาด Client Router Cache ของ Next.js 16+
